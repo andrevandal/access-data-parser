2a0051a2fff08b0a3c5dde256389338b00d233bc
André Van Dal <me@andrevandal.dev>
2019-07-05 17:46:57 -0300

    feat: init accessdata with return standard data

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const renameKeys = require('./lib/renameKeys')
+const pickBy = require('lodash.pickby')
+
+const defaultQueryKeys = {
+  utm_source: 'source',
+  utm_medium: 'medium',
+  utm_campaign: 'campaign',
+  utm_content: 'content',
+  utm_term: 'term',
+  utmSource: 'source',
+  utmMedium: 'medium',
+  utmCampaign: 'campaign',
+  utmContent: 'content',
+  utmTerm: 'term',
+  referral: 'referrer'
+}
+
+const accesData = function(queries) {
+  let {
+    referrer,
+    source,
+    medium,
+    campaign,
+    content,
+    term,
+    gclid,
+    fbclid
+  } = renameKeys(defaultQueryKeys, queries)
+
+  return pickBy({
+    source,
+    medium,
+    campaign,
+    content,
+    term,
+    fbclid,
+    gclid,
+    referrer
+  })
+}
+module.exports = accesData
