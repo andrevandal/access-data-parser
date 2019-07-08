4e56bc174201498de92e2066f974c962588c06f2
André Van Dal <me@andrevandal.dev>
2019-07-08 14:21:13 -0300

    feat: add chanel type in response

--- src/index.js
+++ src/index.js
@@ -86,6 +86,37 @@ const accesData = function(queries) {
     medium = '(not set)'
   }
 
+  let channel
+
+  switch (medium) {
+    case 'sm':
+      channel = 'social'
+      break
+    case '(not set)':
+      channel = 'direct'
+      break
+    case 'organic':
+      channel = 'organic'
+      break
+    case 'e-mail':
+    case 'email':
+      channel = 'email'
+      break
+    case 'cpc':
+    case 'ppc':
+    case 'paidsearch':
+      channel = 'paid'
+      break
+    case 'display':
+    case 'cpm':
+    case 'banner':
+      channel = 'display'
+      break
+    default:
+      channel = medium
+      break
+  }
+
   return pickBy({
     source,
     medium,
@@ -94,7 +125,8 @@ const accesData = function(queries) {
     term,
     fbclid,
     gclid,
-    referrer
+    referrer,
+    channel
   })
 }
 module.exports = accesData
